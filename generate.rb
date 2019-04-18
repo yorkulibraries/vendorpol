eafbe38dbab26cbfa70616d4b97f96fdd96931c6
William Denton <wtd@pobox.com>
2019-04-18 12:39:59 -0400

    Sort vendors and their products

--- generate.rb
+++ generate.rb
@@ -20,10 +20,10 @@ puts <<HEAD
   </p>
 HEAD
 
-vendor_data.each do |vendor|
+vendor_data.sort_by { |v|  v[0] }.each do |vendor|
   puts "<h3> " + vendor[0] + "</h3>"
   puts "  <ul>"
-  vendor[1].each do |product|
+  vendor[1].sort_by { |p| p[0] }.each do |product|
     print "    <li> " + product[0] + ": "
     unless product[1][:privacy_url].nil?
       print %(<a href="#{product[1][:privacy_url]}">privacy</a>)
