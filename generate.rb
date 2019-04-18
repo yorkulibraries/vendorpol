b1f32addd5966353b317b0b6d70551b9832e98e6
William Denton <wtd@pobox.com>
2019-04-18 12:20:39 -0400

    Generate nice HTML, with h3s

--- generate.rb
+++ generate.rb
@@ -1,18 +1,38 @@
 #!/usr/bin/env ruby
 
 require "csv"
+require "json"
 
 data_file = "vendorpol.csv"
 
-puts "<ul>"
+vendor_data = Hash.new { |h, k| h[k] = {} } # A hash of hashes
+
 CSV.foreach(data_file, headers: true, header_converters: :symbol) do |row|
-  print %(<li> #{row[:vendor]}: #{row[:title]}: )
-  unless row[:privacy_url].nil?
-    print %(<a href="#{row[:privacy_url]}">privacy</a> )
-  end
-  unless row[:tc_url].nil?
-    print %(<a href="#{row[:tc_url]}">terms and conditions</a>)
-  end
-  puts %(</li>)
+  vendor_data[row[:vendor]][row[:title]] = {
+    privacy_url: row[:privacy_url],
+    tc_url: row[:tc_url]
+  }
 end
-puts "</ul>"
+
+puts <<HEAD
+  <p>
+    Explanatory text goes here.
+  </p>
+HEAD
+
+vendor_data.each do |vendor|
+  puts "<h3> " + vendor[0] + "</h3>"
+  puts "  <ul>"
+  vendor[1].each do |product|
+    print "    <li> " + product[0] + ": "
+    unless product[1][:privacy_url].nil?
+      print %(<a href="#{product[1][:privacy_url]}">privacy</a>)
+      print ", " unless product[1][:tc_url].nil?
+    end
+    unless product[1][:tc_url].nil?
+      print %(<a href="#{product[1][:tc_url]}">t&amp;c</a>)
+    end
+    puts "</li>"
+  end
+  puts "  </ul>"
+end
